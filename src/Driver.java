aef08da5915914555a13c9dc49e164d6ee10efc1
anirwan <anirwanchowdhury@gmail.com>
2016-04-22 16:04:53 +0800

    First commit

---
+++ src/Driver.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by anirwanchowdhury on 22/04/2016.
+ */
+
+public class Driver {
+
+    public static void main (String[] args) {
+        List<String> hexagons = new ArrayList<String>();
+
+        hexagons.add("EACFBD");
+        hexagons.add("DFECAB");
+        hexagons.add("FEDABC");
+        hexagons.add("AFDEBC");
+        hexagons.add("DCBAFE");
+        hexagons.add("ACBEFD");
+        hexagons.add("AFEDCB");
+
+    }
+}
