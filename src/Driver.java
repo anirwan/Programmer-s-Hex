240d963154f9959ce4854596516c77f40339720d
anirwan <anirwanchowdhury@gmail.com>
2016-04-30 23:27:16 +0800

    Updated program with rotate()

--- src/Driver.java
+++ src/Driver.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class Driver {
     private static Hexagon[] hexagons;
+    private static int count = 0;
 
     static ArrayList<Hexagon> potentialHexagons(ArrayList<Hexagon> currentHexagons) {
         int numberOfHexagons = currentHexagons.size();
@@ -47,16 +48,43 @@ public class Driver {
 
     static void placePieces(ArrayList<Hexagon> currentHexagons) {
         if(currentHexagons.size() == 7) {
-            System.out.println(currentHexagons.toString());
+            count++;
+            System.out.println(count + ". " + currentHexagons.toString());
         } else {
             ArrayList<Hexagon> potentialHexagons = potentialHexagons(currentHexagons);
             
             for(Hexagon hex : potentialHexagons) {
                 ArrayList<Hexagon> temp = (ArrayList<Hexagon>) currentHexagons.clone();
+                if(!currentHexagons.isEmpty()) {
+                    hex = rotate(currentHexagons, hex);
+                }
                 temp.add(hex);
                 placePieces(temp);
             }
         }
+    }
+    
+    static Hexagon rotate(ArrayList<Hexagon> currentHexagons, Hexagon hex) {
+        int piecePosition;
+        int offset = 3;
+        char hubValue = currentHexagons.get(0).getValue(currentHexagons.size() - 1);
+        int hubPosition = currentHexagons.get(0).getPosition(hubValue);
+
+        if(hubPosition < 3) {
+            piecePosition = hubPosition + offset;
+        } else {
+            piecePosition = hubPosition - offset;
+        }
+
+        while (hex.getPosition(hubValue) != piecePosition) {
+            char[] temp = new char[6];
+            temp[0] = hex.sides[5];
+            System.arraycopy(hex.sides, 0, temp, 1, 5);
+            hex.sides = temp;
+            rotate(currentHexagons, hex);
+        }
+
+        return hex;
     }
 
     public static void main (String[] args) {
@@ -68,35 +96,37 @@ public class Driver {
         System.out.println("2. Input new hexagons");
         System.out.print("Please choose an option: ");
         input = scan.nextInt();
+        System.out.println();
 
         switch(input) {
             case 1:
                 hexagons = new Hexagon[] {
-                        new Hexagon(new char[] {'E', 'A', 'C', 'F', 'B', 'D'}),
-                        new Hexagon(new char[] {'D', 'F', 'E', 'C', 'A', 'B'}),
-                        new Hexagon(new char[] {'F', 'E', 'D', 'A', 'B', 'C'}),
-                        new Hexagon(new char[] {'A', 'F', 'D', 'E', 'B', 'C'}),
-                        new Hexagon(new char[] {'D', 'C', 'B', 'A', 'E', 'F'}),
-                        new Hexagon(new char[] {'A', 'C', 'B', 'E', 'F', 'D'}),
-                        new Hexagon(new char[] {'A', 'F', 'E', 'D', 'C', 'B'})
+                        new Hexagon(new char[] {'D', 'B', 'F', 'A', 'C', 'E'}),
+                        new Hexagon(new char[] {'F', 'C', 'E', 'D', 'A', 'B'}),
+                        new Hexagon(new char[] {'C', 'F', 'A', 'D', 'B', 'E'}),
+                        new Hexagon(new char[] {'D', 'E', 'B', 'A', 'C', 'F'}),
+                        new Hexagon(new char[] {'A', 'C', 'E', 'F', 'D', 'B'}),
+                        new Hexagon(new char[] {'D', 'C', 'B', 'E', 'F', 'A'}),
+                        new Hexagon(new char[] {'B', 'A', 'E', 'D', 'F', 'C'})
                 };
                 break;
             case 2:
                 hexagons = new Hexagon[7];
                 System.out.print("Please enter the first hex: ");
-                hexagons[0] = new Hexagon(scan.nextLine().toCharArray());
+                hexagons[0] = new Hexagon(scan.next().toCharArray());
                 System.out.print("Please enter the second hex: ");
-                hexagons[1] = new Hexagon(scan.nextLine().toCharArray());
+                hexagons[1] = new Hexagon(scan.next().toCharArray());
                 System.out.print("Please enter the third hex: ");
-                hexagons[2] = new Hexagon(scan.nextLine().toCharArray());
+                hexagons[2] = new Hexagon(scan.next().toCharArray());
                 System.out.print("Please enter the fourth hex: ");
-                hexagons[3] = new Hexagon(scan.nextLine().toCharArray());
+                hexagons[3] = new Hexagon(scan.next().toCharArray());
                 System.out.print("Please enter the fifth hex: ");
-                hexagons[4] = new Hexagon(scan.nextLine().toCharArray());
+                hexagons[4] = new Hexagon(scan.next().toCharArray());
                 System.out.print("Please enter the sixth hex: ");
-                hexagons[5] = new Hexagon(scan.nextLine().toCharArray());
+                hexagons[5] = new Hexagon(scan.next().toCharArray());
                 System.out.print("Please enter the seventh hex: ");
-                hexagons[6] = new Hexagon(scan.nextLine().toCharArray());
+                hexagons[6] = new Hexagon(scan.next().toCharArray());
+                System.out.println();
                 break;
             default:
                 System.out.println("Incorrect choice!");
@@ -104,5 +134,6 @@ public class Driver {
         }
         scan.close();
         placePieces(new ArrayList<Hexagon>());
+        System.out.println("\nTotal possible solutions: " + count);
     }
 }
